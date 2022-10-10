a6736a02452eb7c2f5de794927b2fc9bca2c848c
Cameron <c.gourley03@gmail.com>
2022-10-10 20:53:33 +0000

    Add data id to cards to check if they match in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,29 @@
 let cards = document.querySelectorAll('.card');
 
+let cardFlipped = false;
+let firstChoice, secondChoice;
+
 function flipCard() {
+    if (this === firstChoice) return;
+
     this.classList.toggle('flip');
+
+    if (!cardFlipped) {
+        cardFlipped = true;
+        firstChoice = this;
+
+        return;
+    }
+
+    secondChoice = this;
+
+    check();
+}
+
+function check() {
+    let match = firstChoice === secondChoice;
+
+    console.log('match');
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard) )
